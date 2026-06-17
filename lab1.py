c507e1da48a0588f577bec2f9336898344e95414
lekanodutay75 <lekanodutayo@outlook.com>
2026-06-17 17:10:42 -0400

    message

--- lab1.py
+++
@@ -1,5 +0,0 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-X: int = 10 + 10
-logging.info('The value of X is: %d', X)
--- lab3.py
+++
@@ -1,6 +0,0 @@
-my_duct = {
-    'fname' : 'mozeed',
-    'lname' : 'odutayo'
-
-}
-print(my_dict)
